1c00de54ad574d39222dc88547a20ea617907e3d
phamtung86 <139665713+phamtung86@users.noreply.github.com>
2023-12-05 12:27:27 +0700

    code_first

---
+++ index.js
@@ -0,0 +1,18 @@
+var btn = document.getElementsByClassName("account--click");
+btn[0].addEventListener("click",function () {
+    var user = document.getElementById("account--user").value;
+    var password = document.getElementById("account--password").value;
+    console.log(user);
+    console.log(password);
+    let count;
+    if(user == "nguyenthilananh" && password == "27092004"){
+        confirm("Bạn giỏi quá cùng đi tiếp nhé");
+        count = 1;
+        if(count == 1){
+            window.open('');
+        }
+    }
+    else {
+        alert("Sai rồi huhu ( Gợi ý: Những gì liên quan đến bản thân bạn )")
+    }
+})
